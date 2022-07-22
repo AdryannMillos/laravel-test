d75753553cd899980eeb6b6cde6fc1a019edd9ed
Adryann Millos <adryann312@gmail.com>
2022-07-22 08:19:09 -0300

    create modules

--- backend/Modules/Users/Routes/api.php
+++ backend/Modules/Users/Routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('auth/login', [AuthController::class, 'login']);
-
+Route::middleware('auth:api')->get('/users', function (Request $request) {
+    return $request->user();
+});
